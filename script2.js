e04f8b548fb92e2579de57c18da97e8c0ce7e256
root-astra <utp0141011@alumno.utpuebla.edu.mx>
2024-07-28 19:43:53 -0600

    Repositorio base API web de cocteles de bebida automatizado

---
+++ script2.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetchCocktails();
+});
+
+function fetchCocktails() {
+    const apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=tequila';
+    
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const drinks = data.drinks;
+            const cocktailMenu = document.getElementById('cocktail-menu');
+            drinks.forEach(drink => {
+                const cocktailElement = document.createElement('div');
+                cocktailElement.classList.add('cocktail');
+                cocktailElement.innerHTML = `
+                    <img src="${drink.strDrinkThumb}" alt="${drink.strDrink}">
+                    <h2>${drink.strDrink}</h2>
+                    <p>${drink.strInstructions}</p>
+                `;
+                cocktailMenu.appendChild(cocktailElement);
+            });
+        })
+        .catch(error => console.error('Error al cargar los cócteles:', error));
+}
